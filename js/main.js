f80602d73bf492eb87f396f31eec23aa730a4dbf
Randy MacLean <randy_maclean@hotmail.ca>
2022-07-07 11:56:12 -0700

    added event listeners to arrow keys, can now navigate playing board

--- js/main.js
+++ js/main.js
@@ -2,3 +2,32 @@ const timeLeftDisplay = document.getElementById('time-left');
 const resultDisplay = document.getElementById('result');
 const startPauseButton = document.getElementById('start-pause-button');
 const squares = document.querySelectorAll('.grid div');
+const width = 9;
+let currentIndex = 76;
+
+
+const moveFrog = (e) => {
+
+    switch(e.key) {
+        case 'ArrowLeft' :
+            console.log("move left");
+            currentIndex -= 1;
+            break
+        case 'ArrowRight' :
+            console.log("move right");
+            currentIndex += 1;
+            break
+        case 'ArrowUp' :
+            console.log("move up");
+            currentIndex -= width;
+            break
+        case 'ArrowDown' :
+            console.log("move down");
+            currentIndex += width;
+            break
+    }
+
+    squares[currentIndex].classList.add('frog');
+};
+
+document.addEventListener("keyup", moveFrog);
