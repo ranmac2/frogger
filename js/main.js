1643979a474d3dc18f76425f45e6331ad71b9987
Randy MacLean <randy_maclean@hotmail.ca>
2022-07-07 11:21:39 -0700

    added styling to grid squares and created variables in js file

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const timeLeftDisplay = document.getElementById('time-left');
+const resultDisplay = document.getElementById('result');
+const startPauseButton = document.getElementById('start-pause-button');
+const squares = document.querySelectorAll('.grid div');
