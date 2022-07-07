c07b4c2c5475710fbffb8c81a6544d84ad0a2d95
Randy MacLean <randy_maclean@hotmail.ca>
2022-07-07 12:09:29 -0700

    Added boundary restrictions to playing grid using js file

--- js/main.js
+++ js/main.js
@@ -8,24 +8,26 @@ let currentIndex = 76;
 
 const moveFrog = (e) => {
 
+    squares[currentIndex].classList.remove('frog');
+
     switch(e.key) {
         case 'ArrowLeft' :
             console.log("move left");
-            currentIndex -= 1;
+            if (currentIndex % width !== 0) currentIndex -= 1;
             break
         case 'ArrowRight' :
             console.log("move right");
-            currentIndex += 1;
+            if (currentIndex % width < width - 1) currentIndex += 1;
             break
         case 'ArrowUp' :
             console.log("move up");
-            currentIndex -= width;
+            if (currentIndex - width >= 0) currentIndex -= width;
             break
         case 'ArrowDown' :
             console.log("move down");
-            currentIndex += width;
+            if (currentIndex + width < width * width) currentIndex += width;
             break
-    }
+    };
 
     squares[currentIndex].classList.add('frog');
 };
