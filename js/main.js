e063c097b6b7551c2a2b06a55545c131e7ac9a51
Randy MacLean <randy_maclean@hotmail.ca>
2022-07-08 09:24:48 -0700

    top and bottom log rows are now moving

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const squares = document.querySelectorAll('.grid div');
 const width = 9;
 let currentIndex = 76;
 const logsLeft = document.querySelectorAll('.log-left');
+const logsRight = document.querySelectorAll('.log-right');
+
 
 
 const moveFrog = (e) => {
@@ -35,7 +37,7 @@ const moveFrog = (e) => {
 
 document.addEventListener("keyup", moveFrog);
 
-const autoMoveLogs = () => {
+const autoMoveLogsLeft = () => {
     logsLeft.forEach(logLeft => moveLogLeft(logLeft));
 }
 
@@ -64,4 +66,35 @@ const moveLogLeft = (logLeft) => {
     }
 }
 
-setInterval(autoMoveLogs, 1000);
+setInterval(autoMoveLogsLeft, 1000);
+
+const autoMoveLogsRight = () => {
+    logsRight.forEach(logRight => moveLogRight(logRight));
+}
+
+const moveLogRight = (logRight) => {
+    switch(true) {
+        case logRight.classList.contains('l5') :
+            logRight.classList.remove('l5')
+            logRight.classList.add('l4')
+            break
+        case logRight.classList.contains('l4') :
+            logRight.classList.remove('l4')
+            logRight.classList.add('l3')
+            break
+         case logRight.classList.contains('l3') :
+            logRight.classList.remove('l3')
+            logRight.classList.add('l2')
+            break
+        case logRight.classList.contains('l2') :
+            logRight.classList.remove('l2')
+            logRight.classList.add('l1')
+            break
+        case logRight.classList.contains('l1') :
+            logRight.classList.remove('l1')
+            logRight.classList.add('l5')
+            break
+    }
+}
+
+setInterval(autoMoveLogsRight, 1000);
