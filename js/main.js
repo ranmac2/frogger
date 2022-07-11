3f9488ff77a340a058f124756d1b94bb025cf9a7
Randy MacLean <randy_maclean@hotmail.ca>
2022-07-11 11:26:27 -0700

    added cars, logs, and frog images

--- js/main.js
+++ js/main.js
@@ -1,179 +1,178 @@
-const timeLeftDisplay = document.getElementById('time-left');
-const resultDisplay = document.getElementById('result');
-const startPauseButton = document.getElementById('start-pause-button');
-const squares = document.querySelectorAll('.grid div');
+const timeLeftDisplay = document.getElementById("time-left");
+const resultDisplay = document.getElementById("result");
+const startPauseButton = document.getElementById("start-pause-button");
+const squares = document.querySelectorAll(".grid div");
 const width = 9;
 let currentIndex = 76;
-const logsLeft = document.querySelectorAll('.log-left');
-const logsRight = document.querySelectorAll('.log-right');
-const carsLeft = document.querySelectorAll('.car-left');
-const carsRight = document.querySelectorAll('.car-right');
+const logsLeft = document.querySelectorAll(".log-left");
+const logsRight = document.querySelectorAll(".log-right");
+const carsLeft = document.querySelectorAll(".car-left");
+const carsRight = document.querySelectorAll(".car-right");
 let timerId;
 let outcomeTimerId;
 let currentTime = 20;
 
 const moveFrog = (e) => {
+  squares[currentIndex].classList.remove("frog");
 
-    squares[currentIndex].classList.remove('frog');
+  switch (e.key) {
+    case "ArrowLeft":
+      console.log("move left");
+      if (currentIndex % width !== 0) currentIndex -= 1;
+      break;
+    case "ArrowRight":
+      console.log("move right");
+      if (currentIndex % width < width - 1) currentIndex += 1;
+      break;
+    case "ArrowUp":
+      console.log("move up");
+      if (currentIndex - width >= 0) currentIndex -= width;
+      break;
+    case "ArrowDown":
+      console.log("move down");
+      if (currentIndex + width < width * width) currentIndex += width;
+      break;
+  }
 
-    switch(e.key) {
-        case 'ArrowLeft' :
-            console.log("move left");
-            if (currentIndex % width !== 0) currentIndex -= 1;
-            break
-        case 'ArrowRight' :
-            console.log("move right");
-            if (currentIndex % width < width - 1) currentIndex += 1;
-            break
-        case 'ArrowUp' :
-            console.log("move up");
-            if (currentIndex - width >= 0) currentIndex -= width;
-            break
-        case 'ArrowDown' :
-            console.log("move down");
-            if (currentIndex + width < width * width) currentIndex += width;
-            break
-    };
-
-    squares[currentIndex].classList.add('frog');
+  squares[currentIndex].classList.add("frog");
 };
 
 document.addEventListener("keyup", moveFrog);
 
 const autoMoveElements = () => {
-    currentTime--;
-    timeLeftDisplay.textContent = currentTime;
-    logsLeft.forEach(logLeft => moveLogLeft(logLeft));
-    logsRight.forEach(logRight => moveLogRight(logRight));
-    carsLeft.forEach(carLeft => moveCarLeft(carLeft));
-    carsRight.forEach(carRight => moveCarRight(carRight));
+  currentTime--;
+  timeLeftDisplay.textContent = currentTime;
+  logsLeft.forEach((logLeft) => moveLogLeft(logLeft));
+  logsRight.forEach((logRight) => moveLogRight(logRight));
+  carsLeft.forEach((carLeft) => moveCarLeft(carLeft));
+  carsRight.forEach((carRight) => moveCarRight(carRight));
 };
 
 const checkOutcome = () => {
-    gameLoss();
-    gameWin();
+  gameLoss();
+  gameWin();
 };
 
 const moveLogLeft = (logLeft) => {
-    switch(true) {
-        case logLeft.classList.contains('l1') :
-            logLeft.classList.remove('l1')
-            logLeft.classList.add('l2')
-            break
-        case logLeft.classList.contains('l2') :
-            logLeft.classList.remove('l2')
-            logLeft.classList.add('l3')
-            break
-         case logLeft.classList.contains('l3') :
-            logLeft.classList.remove('l3')
-            logLeft.classList.add('l4')
-            break
-        case logLeft.classList.contains('l4') :
-            logLeft.classList.remove('l4')
-            logLeft.classList.add('l5')
-            break
-        case logLeft.classList.contains('l5') :
-            logLeft.classList.remove('l5')
-            logLeft.classList.add('l1')
-            break
-    }
+  switch (true) {
+    case logLeft.classList.contains("l1"):
+      logLeft.classList.remove("l1");
+      logLeft.classList.add("l2");
+      break;
+    case logLeft.classList.contains("l2"):
+      logLeft.classList.remove("l2");
+      logLeft.classList.add("l3");
+      break;
+    case logLeft.classList.contains("l3"):
+      logLeft.classList.remove("l3");
+      logLeft.classList.add("l4");
+      break;
+    case logLeft.classList.contains("l4"):
+      logLeft.classList.remove("l4");
+      logLeft.classList.add("l5");
+      break;
+    case logLeft.classList.contains("l5"):
+      logLeft.classList.remove("l5");
+      logLeft.classList.add("l1");
+      break;
+  }
 };
 
 const moveLogRight = (logRight) => {
-    switch(true) {
-        case logRight.classList.contains('l5') :
-            logRight.classList.remove('l5')
-            logRight.classList.add('l4')
-            break
-        case logRight.classList.contains('l4') :
-            logRight.classList.remove('l4')
-            logRight.classList.add('l3')
-            break
-         case logRight.classList.contains('l3') :
-            logRight.classList.remove('l3')
-            logRight.classList.add('l2')
-            break
-        case logRight.classList.contains('l2') :
-            logRight.classList.remove('l2')
-            logRight.classList.add('l1')
-            break
-        case logRight.classList.contains('l1') :
-            logRight.classList.remove('l1')
-            logRight.classList.add('l5')
-            break
-    }
+  switch (true) {
+    case logRight.classList.contains("l5"):
+      logRight.classList.remove("l5");
+      logRight.classList.add("l4");
+      break;
+    case logRight.classList.contains("l4"):
+      logRight.classList.remove("l4");
+      logRight.classList.add("l3");
+      break;
+    case logRight.classList.contains("l3"):
+      logRight.classList.remove("l3");
+      logRight.classList.add("l2");
+      break;
+    case logRight.classList.contains("l2"):
+      logRight.classList.remove("l2");
+      logRight.classList.add("l1");
+      break;
+    case logRight.classList.contains("l1"):
+      logRight.classList.remove("l1");
+      logRight.classList.add("l5");
+      break;
+  }
 };
 
 const moveCarLeft = (carLeft) => {
-    switch(true) {
-        case carLeft.classList.contains('c1') :
-             carLeft.classList.remove('c1')
-             carLeft.classList.add('c2')
-             break
-        case carLeft.classList.contains('c2') :
-             carLeft.classList.remove('c2')
-             carLeft.classList.add('c3')
-             break
-        case carLeft.classList.contains('c3') :
-             carLeft.classList.remove('c3')
-             carLeft.classList.add('c1')
-             break
-    }
+  switch (true) {
+    case carLeft.classList.contains("c1"):
+      carLeft.classList.remove("c1");
+      carLeft.classList.add("c2");
+      break;
+    case carLeft.classList.contains("c2"):
+      carLeft.classList.remove("c2");
+      carLeft.classList.add("c3");
+      break;
+    case carLeft.classList.contains("c3"):
+      carLeft.classList.remove("c3");
+      carLeft.classList.add("c1");
+      break;
+  }
 };
 
 const moveCarRight = (carRight) => {
-    switch(true) {
-        case carRight.classList.contains('c3') :
-             carRight.classList.remove('c3')
-             carRight.classList.add('c2')
-             break
-        case carRight.classList.contains('c2') :
-             carRight.classList.remove('c2')
-             carRight.classList.add('c1')
-             break
-        case carRight.classList.contains('c1') :
-             carRight.classList.remove('c1')
-             carRight.classList.add('c3')
-             break
-    }
+  switch (true) {
+    case carRight.classList.contains("c3"):
+      carRight.classList.remove("c3");
+      carRight.classList.add("c2");
+      break;
+    case carRight.classList.contains("c2"):
+      carRight.classList.remove("c2");
+      carRight.classList.add("c1");
+      break;
+    case carRight.classList.contains("c1"):
+      carRight.classList.remove("c1");
+      carRight.classList.add("c3");
+      break;
+  }
 };
 
 const gameLoss = () => {
-    if (
-        squares[currentIndex].classList.contains('c1') ||
-        squares[currentIndex].classList.contains('l4') ||
-        squares[currentIndex].classList.contains('l5') ||
-        currentTime <= 0
-        ) {
-        resultDisplay.textContent = 'You Lose!';
-        clearInterval(timerId);
-        squares[currentIndex].classList.remove('frog');
-        document.removeEventListener('keyup', moveFrog);
-    }
+  if (
+    squares[currentIndex].classList.contains("c1") ||
+    squares[currentIndex].classList.contains("l4") ||
+    squares[currentIndex].classList.contains("l5") ||
+    currentTime <= 0
+  ) {
+    resultDisplay.textContent = "You Lose!";
+    clearInterval(timerId);
+    squares[currentIndex].classList.remove("frog");
+    document.removeEventListener("keyup", moveFrog);
+  }
 };
 
 const gameWin = () => {
-    if (squares[currentIndex].classList.contains('ending-block')) {
-        resultDisplay.textContent = 'You Win!';
-        clearInterval(timerId);
-        document.removeEventListener('keyup', moveFrog);
-    }
+  if (squares[currentIndex].classList.contains("ending-block")) {
+    resultDisplay.textContent = "You Win!";
+    clearInterval(timerId);
+    document.removeEventListener("keyup", moveFrog);
+  }
 };
 
 if (!timerId) {
-    document.removeEventListener('keyup', moveFrog);
+  document.removeEventListener("keyup", moveFrog);
 }
 
-startPauseButton.addEventListener('click', () => {
-    if (timerId) {
-        clearInterval(timerId)
-        clearInterval(outcomeTimerId)
-        document.removeEventListener('keyup', moveFrog);
-        timerId = null;
-        outcomeTimerId = null;
-    } else {
-        timerId = setInterval(autoMoveElements, 1000)
-        outcomeTimerId = setInterval(checkOutcome, 1);
-        document.addEventListener('keyup', moveFrog);
-    }
+startPauseButton.addEventListener("click", () => {
+  if (timerId) {
+    clearInterval(timerId);
+    clearInterval(outcomeTimerId);
+    document.removeEventListener("keyup", moveFrog);
+    timerId = null;
+    outcomeTimerId = null;
+  } else {
+    timerId = setInterval(autoMoveElements, 1000);
+    outcomeTimerId = setInterval(checkOutcome, 1);
+    document.addEventListener("keyup", moveFrog);
+  }
 });
